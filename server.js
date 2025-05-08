a0b1e24b5ae696ea0f9f530c06c225e5704dd261
OceanGonen <ocean.gonen@hotmail.com>
2025-05-08 15:07:08 +0200

    database gelinked, es lint / pretttier installed

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const session = require('express-session')
-const port = 3000
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const port = 3000;
 
 app
-    .use('/static', express.static('static'))
-    .use(express.urlencoded({ extended: true }))
-    .use(session({
-        resave: false,
+  .use("/static", express.static("static"))
+  .use(express.urlencoded({ extended: true }))
+  .use(
+    session({
+      resave: false,
 
-        saveUninitialized: true,
+      saveUninitialized: true,
 
-        secret: process.env.SESSION_SECRET
-    }))
+      secret: process.env.SESSION_SECRET,
+    }),
+  )
 
-    .set('view engine', 'ejs')
-    .set('views', 'view')
+  .set("view engine", "ejs")
+  .set("views", "view")
 
-    .get('/', onHome)
+  .get("/", loadHome)
 
+  .listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
 
-
-
-    .listen(port, () => {
-        console.log(`Server running at http://localhost:${port}`);
-    });
-
-function onHome(req, res) {
-    req.session.userID = 95234;
-    let userID = req.session.userID; 
-    res.render('index.ejs', {userID});
+function loadHome(req, res) {
+  req.session.userID = 95234;
+  let userID = req.session.userID;
+  res.render("index.ejs", { userID });
 }
+
+let Testtest = 4;
